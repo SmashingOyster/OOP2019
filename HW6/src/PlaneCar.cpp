461e4fe4980ddacd3f38902cde13beb1835601bc
SmashingOyster <45uiz@gmail.com>
2019-04-29 15:09:30 -0600

    HW6

--- HW6/src/PlaneCar.cpp
+++ HW6/src/PlaneCar.cpp
@@ -8,27 +8,44 @@ namespace vehicle{
   PlaneCar::PlaneCar(Color _color, Owner _owner, bool _keys)
     : Car(_color, _owner, _keys), Plane(_color, _owner, _keys)
     {
-      std::cout << "Doing some cool Engineering.. Constructing PlaneCar" << std::endl;
+      std::cout << "Doing some cool Engineering.. Constructing PlaneCar \n" << std::endl;
+      std::cout << Plane::getOwner() << " is the owner \n"
+      << " they take priority and we paint it " << Plane::getColor() << std::endl;
+    }
+    PlaneCar::menu() const{
+      std::cout<< "Welcome to the PlaneCar UserInterface, \n"
+      << "To use the plane mode, press 1 \n"
+      << "To use the car mode, press 0 \n"
+      << "Press 3 to deactivate engine.." << std::endl;
     }
 
     void PlaneCar::drive() const override{
       int choice;
-      string menu = std::cout<< "Welcome to the PlaneCar UserInterface, \n"
-      << "To use the plane mode, press 1 \n"
-      << "To use the car mode, press 0 \n"
-      << "Press 3 to deactivate engine.." << std::endl;
+      while(choice != 3){
+      menu();
 
       std::cin >> choice;
       if(choice == 1){
         std::system("cls");
         std::cout << "Entering Plane Mode... Deploying Jets.... \n" << std::endl;
-        std:: cout << menu << std::endl;
+        Plane::drive();
+        menu();
       }
       else if(choice == 0){
+        std::system("cls");
         std::cout << "Entering Car Mode... Deploying wheels and deactivating jets..." << std:endl;
+        Car::drive();
+        menu();
       }
       else if(choice == 3){
+        std::system("cls");
         std::cout << "Powering off the PlaneCar.. " << std::endl;
       }
+      else
+        std::cout << "Could not recieve correct input for PlaneCar" << std::endl;
     }
+  }
+  PlaneCar::~PlaneCar(){
+    std::cout << "Salvaging parts from the PlaneCar and taking away keys.. " << std::endl;
+  }
 }
