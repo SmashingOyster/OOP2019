b7a2ca0b460c04a03ede0a00b58a28b4fc29c6df
= <=>
2019-05-16 17:10:24 -0600

    HW4

--- HW4/test/authenTest.js
+++ HW4/test/authenTest.js
@@ -2,12 +2,16 @@
 
 var assert = require('assert');
 var chai = require('chai');
+var chaiChanges = require("chai-changes");
+chai.use(chaiChanges);
+var transaction = require('../src/transaction');
 
 describe("Citizen", function(){
   describe("constructor()", function(){
     it("Should create a citizen with an Authentication number 001", function(){
       let Philip = new transaction.Citizen("Citizen", "Philip Philipson", 001);
       assert.equal(001, Philip.authNum);
+
     });
   });
 });
